56bc5224c338e6abaec9eebc60405abe6c51308c
Mathieu Seguin <mathieu.seguin@gmail.com>
2016-02-24 11:25:53 -0800

    Added Uniteus::Prime.first

--- lib/uniteus/prime.rb
+++ lib/uniteus/prime.rb
@@ -1,4 +1,35 @@
 module Uniteus
   class Prime
+    class << self
+      def first(i = 1)
+        i = i.to_i
+        
+        # Estimate the size of the nth primes
+        case
+          when i < 0
+            return []
+          when i <= 5
+            n = i * 2 + 1
+          when i > 5
+            n = i * Math.log(i) + i * Math.log(Math.log(i))
+        end
+
+        # Implementation of the Sieves of Eratosthenes
+        # see: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+        primes = (0..n).to_a
+        primes[0] = primes[1] = nil
+
+        primes.each do |p|
+          next unless p
+          break if p * p > n
+
+          (p * p).step(n, p) do |m|
+            primes[m] = nil
+          end
+        end
+
+        primes.compact.first(i)
+      end
+    end
   end
 end
--- spec/uniteus/prime_spec.rb
+++ spec/uniteus/prime_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 RSpec.describe Uniteus::Prime do
+  let(:first_10_primes) { [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] }
+
+  describe '.first' do
+    it { expect(Uniteus::Prime.first(0)).to eq([]) }
+    it { expect(Uniteus::Prime.first).to eq(first_10_primes.slice(0, 1)) }
+    it { expect(Uniteus::Prime.first(10)).to eq(first_10_primes) }
+    it { expect(Uniteus::Prime.first("10")).to eq(first_10_primes) }
+  end
 end
